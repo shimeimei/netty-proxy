662814f01f00abe8d01d7ffacb00e43b7cbaafd0
smm <imshimeimei@gmail.com>
2019-01-10 17:24:10 +0800

    【提交类型】:阶段性提交 【问题描述】: 新建工程 【修改人】: 史美美 【修改时间】:2019/1/10

---
+++ src/main/java/startApplication.java
@@ -0,0 +1,11 @@
+/**
+ * Project <netty-proxy>
+ * Created by smm on 2019/1/10 17:20.
+ */
+public class startApplication {
+
+
+    public static void main(String[] args) {
+        System.out.println("this is applicaiton");
+    }
+}
