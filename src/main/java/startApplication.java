fc4b4ec5cfa52abbd602d25fbe47fbba94735bcc
smm <imshimeimei@gmail.com>
2019-02-11 16:16:30 +0800

    【提交类型】:阶段性提交 【问题描述】: 新建工程 【修改人】: 史美美 【修改时间】:2019/1/10

--- src/main/java/startApplication.java
+++ src/main/java/startApplication.java
@@ -1,3 +1,13 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import proxy.HttpProxyServerHandle;
+
 /**
  * Project <netty-proxy>
  * Created by smm on 2019/1/10 17:20.
@@ -6,6 +16,34 @@ public class startApplication {
 
 
     public static void main(String[] args) {
-        System.out.println("this is applicaiton");
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 100)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<Channel>() {
+
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ch.pipeline().addLast("httpCodec",new HttpServerCodec());
+                            ch.pipeline().addLast("httpObject",new HttpObjectAggregator(65536));
+                            ch.pipeline().addLast("serverHandle",new HttpProxyServerHandle());
+                        }
+                    });
+            ChannelFuture f = b
+                    .bind(843)
+                    .sync();
+            f.channel().closeFuture().sync();
+            System.out.println("netty 代理服务启动------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 }
